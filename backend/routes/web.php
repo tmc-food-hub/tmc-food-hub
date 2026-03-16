c3dd236cad80d0df2799a2099011ef551df59041
Rymndkraft <mstamaria.a12241624@umak.edu.ph>
2026-03-16 16:55:00 +0800

    route adminer to web/public

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/adminer', function () {
+    ob_start();
+    include __DIR__ . '/../public/adminer-wrapper.php';
+    return ob_get_clean();
+});
